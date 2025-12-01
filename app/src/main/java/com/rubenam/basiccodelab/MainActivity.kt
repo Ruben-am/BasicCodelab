0250ca29ac62e37c3a4e625098988834739f79a4
Ruben-am <ru.alba.dev@gmail.com>
2025-12-01 10:25:23 +0100

    add dark mode preview

--- app/src/main/java/com/rubenam/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/rubenam/basiccodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rubenam.basiccodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -164,5 +165,19 @@ fun GreetingsPreview() {
 fun MyappPreview() {
     BasicCodelabTheme {
         myApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingPreview() {
+    BasicCodelabTheme {
+        Greetings()
     }
 }
